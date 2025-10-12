3cbdb4579cb548a1f58433baabf39f0e13ec2ada
Matvej Sobolev <matvej.sobolev.09@gmail.com>
2025-10-12 23:00:43 +0400

    Проверка и возврат книг

--- library/venv/library.py
+++ library/venv/library.py
@@ -1,3 +1,4 @@
+from user import User
 class Library:
   def __init__(self):
     self.books = [] 
@@ -17,16 +18,36 @@ class Library:
       print(f"{book} у {user.name}")
 
   def rent_book(self, book_title, user):
-    #проверка на доступность выдачи книги
-    for book in self.books:
-      if book.title == book_title:
-        self.books.remove(book)
-        self.borrowed_books[book] = user
-        user.borrowed_books.append(book)
-        print(f"Книга {book_title} выдана {user.name}")
+    book_amount = sum(1 for value in self.borrowed_books.values() if value == user)
+    if user.max_books <= book_amount:
+        print(f'{user.name} взял(а) слишком много книг!')
         return
-    print('Такой книги нет')
 
+    #проверка на доступность выдачи книги
+
+    for book in self.books:
+        if book.title == book_title:
+            self.books.remove(book)
+            self.borrowed_books[book.title] = user
+
+            user.borrowed_books.append(book)
+            print(f"Книга {book_title} выдана {user.name}")
+
+            return
+
+    print('Такой книги нет')
+  def return_book(self, book, user):
+
+      if book not in self.borrowed_books:
+          print("Эта книга не была выдана!")
+          return
+
+      del self.borrowed_books[book]
+      self.books.append(book)
+      for i in user.borrowed_books:
+          if i.title == book:
+            user.borrowed_books.remove(i)
+      print(f"Книга '{book}' успешно возвращена!")
   # def  return_book - вернуть книгу
   # def  is_borrowed_book - проверить на наличие книги 
   
